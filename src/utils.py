ec8bae3021b8ff48eb5eff108c43b217091ecfd3
fdevi3-mle <fdevi3@proton.me>
2025-02-05 10:24:04 +0100

    Paths mayhem

---
+++ src/utils.py
@@ -0,0 +1,23 @@
+##Constants & FILEPATHS
+import logging
+import os
+
+CURRENT_PATH = os.path.dirname(os.path.abspath(__file__)) ## src
+ROOT_PATH = os.path.dirname(CURRENT_PATH)
+LOG_PATH = os.path.join(ROOT_PATH, 'logs')
+
+
+
+
+
+
+## Lets test the normal logger instead of the zenml one
+def generate_logger(logpath=LOG_PATH):
+    os.makedirs(logpath,exist_ok=True)
+    logger = logging.getLogger(__name__)
+    print(logpath)
+    filename= 'test-logger.log'
+    filepath = os.path.join(logpath, filename)
+    logging.basicConfig(filename=filepath, encoding='utf-8', level=logging.DEBUG)
+    print(filepath)
+    logger.info("Hi")
--- test-zenml.py
+++ test-zenml.py
@@ -1,26 +1,8 @@
 import logging
 import os.path
 from zenml import pipeline, step
-##Constants & FILEPATHS
-CURRENT_PATH = os.path.dirname(os.path.abspath(__file__)) ## src
-ROOT_PATH = os.path.dirname(CURRENT_PATH)
-LOG_PATH = os.path.join(ROOT_PATH, 'logs')
 
-
-
-
-
-
-## Lets test the normal logger instead of the zenml one
-def generate_logger(logpath=LOG_PATH):
-    os.makedirs(logpath,exist_ok=True)
-    logger = logging.getLogger(__name__)
-    print(logpath)
-    filename= 'test-logger.log'
-    filepath = os.path.join(logpath, filename)
-    logging.basicConfig(filename=filepath, encoding='utf-8', level=logging.DEBUG)
-    print(filepath)
-    logger.info("Hi")
+from src.utils import CURRENT_PATH, ROOT_PATH, generate_logger
 
 
 @step  # Just add this decorator
