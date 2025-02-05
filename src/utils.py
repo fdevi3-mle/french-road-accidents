c5aa96eb88cee3ed7e46705fd7cc89bd354e9b25
fdevi3-mle <fdevi3@proton.me>
2025-02-05 12:24:20 +0100

    Now pls

--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,7 @@ import os
 
 CURRENT_PATH = os.path.dirname(os.path.abspath(__file__)) ## src
 ROOT_PATH = os.path.dirname(CURRENT_PATH)
-LOG_PATH = os.path.join(ROOT_PATH, 'logs')
+LOG_PATH = os.path.join(ROOT_PATH, 'dags')
 
 
 
@@ -16,9 +16,9 @@ def generate_logger(logpath=LOG_PATH):
     os.makedirs(logpath,exist_ok=True)
     logger = logging.getLogger(__name__)
     print(logpath)
-    filename= 'test-logger.log'
+    filename= 'test-logger.pog'
     filepath = os.path.join(logpath, filename)
     logging.basicConfig(filename=filepath, encoding='utf-8', level=logging.DEBUG)
     print(filepath)
     logger.info("Hi")
-    logger.warn("Whatsasasdasd")
+    logger.warning("Whatsasasdasd")
