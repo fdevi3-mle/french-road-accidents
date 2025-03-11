a0c0168fd62841fdff49f7c1321c1f5891f0473c
fdevi3-mle <fdevi3@proton.me>
2025-03-11 22:05:08 +0100

    Reformated the file and added authentication

--- app/app-main.py
+++ app/app-main.py
@@ -2,15 +2,17 @@ import asyncio
 import importlib.util
 import logging
 import os
+import secrets
 import sys
 from contextlib import asynccontextmanager
 from typing import Annotated, Literal
+
 import h3
 import joblib
 from comet_ml.api import API
+from fastapi import Depends, HTTPException, status
 from fastapi import FastAPI, Query
-from fastapi import HTTPException, status
-from fastapi.security import HTTPBasic
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from pydantic import BaseModel, Field
 
 ###Start
@@ -26,7 +28,7 @@ ARIMA_NAME = "ARIMA"
 
 ###PATHS
 CURRENT_PATH = os.path.dirname(os.path.abspath(__file__))
-ZENML_FILE_PATH = os.path.join(CURRENT_PATH,'dummy-retrain.py')
+ZENML_FILE_PATH = os.path.join(CURRENT_PATH, 'dummy-retrain.py')
 
 ###COMET ML API
 api = API(api_key="Xh1kXXM0IIPgqwAP3wTyChS0R")
@@ -36,65 +38,57 @@ model_dic = {}
 
 ##Stuff
 ##Admin Password
-ADMIN_USERNAME='admin'
+ADMIN_USERNAME = 'admin'
 ADMIN_PASSWORD = "admin"
+
 
 ##Pydantic Model
 class ForecastRequest(BaseModel):
     periods: int = Field(100, gt=0, le=366)
 
+
 class ClassifierRequest(BaseModel):
-    vehicle_category: Literal['1','2','3','4','0']
-    obstacle_mobile: Literal['1','2','3','0']
-    impact_point: Literal['1','2','3','4','0']
-    action: Literal['1','2','3','4','0']
-    safety_equipment: Literal['1','2','3','0']
-    road_surface: Literal['1','2','3','0']
-    lum: Literal['1','2','3','4','0']
-    weather: Literal['1','2','3','0']
-    collision_type: Literal['1','2','3','0']
-    speed_limit:int = Field(50, gt=0, le=200)
-    accident_hex_count:int = Field(250, gt=0, le=20000)
+    vehicle_category: Literal['1', '2', '3', '4', '0']
+    obstacle_mobile: Literal['1', '2', '3', '0']
+    impact_point: Literal['1', '2', '3', '4', '0']
+    action: Literal['1', '2', '3', '4', '0']
+    safety_equipment: Literal['1', '2', '3', '0']
+    road_surface: Literal['1', '2', '3', '0']
+    lum: Literal['1', '2', '3', '4', '0']
+    weather: Literal['1', '2', '3', '0']
+    collision_type: Literal['1', '2', '3', '0']
+    speed_limit: int = Field(50, gt=0, le=200)
+    accident_hex_count: int = Field(250, gt=0, le=20000)
     latitude: float = Field(DEFAULT_LAT, ge=-90, le=90)
     longitude: float = Field(DEFAULT_LONG, ge=-180, le=180)
 
+
 class AdminRequest(BaseModel):
-    retrain:bool= Field(False,title="Retraining Trigger")
+    retrain: bool = Field(False, title="Retraining Trigger")
+
 
 ##TODO implement random choice
 def generate_random_classifier_request():
-    return ClassifierRequest(
-        vehicle_category='1',
-        obstacle_mobile='2',
-        impact_point='1',
-        action='1',
-        safety_equipment='2',
-        road_surface='1',
-        lum='2',
-        weather='1',
-        collision_type='1',
-        speed_limit=80,
-        accident_hex_count=250,
-        latitude=0.0,
-        longitude=0.0
-    )
+    return ClassifierRequest(vehicle_category='1', obstacle_mobile='2', impact_point='1', action='1',
+        safety_equipment='2', road_surface='1', lum='2', weather='1', collision_type='1', speed_limit=80,
+        accident_hex_count=250, latitude=0.0, longitude=0.0)
 
-####API and SETUP
+
+####COMET LOAD MODEL and SETUP
 
 def get_gbc_model():
     try:
         api.download_registry_model("fdevi3", "gradientboostingclassifier", output_path=CURRENT_PATH, expand=True,
                                     stage=None)
     except Exception as e:
-        raise HTTPException(status_code=status.HTTP_412_PRECONDITION_FAILED,detail=str(e))
+        raise HTTPException(status_code=status.HTTP_412_PRECONDITION_FAILED, detail=str(e))
+
 
 def get_arima_model():
     try:
-        api.download_registry_model("fdevi3", "forecast-arima-model", output_path=CURRENT_PATH,
-                                    expand=True,
-                                    stage=None)
+        api.download_registry_model("fdevi3", "forecast-arima-model", output_path=CURRENT_PATH, expand=True, stage=None)
     except Exception as e:
-        raise HTTPException(status_code=status.HTTP_412_PRECONDITION_FAILED,detail=str(e))
+        raise HTTPException(status_code=status.HTTP_412_PRECONDITION_FAILED, detail=str(e))
 
 
 ##Startup new
@@ -122,59 +116,56 @@ async def lifespan(app: FastAPI):
     model_dic.clear()
 
 
-#https://fastapi.tiangolo.com/advanced/security/http-basic-auth/#simple-http-basic-auth
+###########API #########################
+# https://fastapi.tiangolo.com/advanced/security/http-basic-auth/#simple-http-basic-auth
 security = HTTPBasic()
 
-app = FastAPI(
-    title="French Road Accidents FAST API Stuff",
-    description= "Franc's rest api stuff for road accident severity predictions",
-    version="1.0.0",
-    lifespan=lifespan,
-    openapi_tags=[
-        {
-            'name':'test',
-            'description': 'Just for testing',
+app = FastAPI(title="French Road Accidents FAST API Stuff",
+    description="Franc's rest api stuff for road accident severity predictions", version="1.0.0", lifespan=lifespan,
+    openapi_tags=[{'name': 'test', 'description': 'Just for testing',
 
-        },
-        {
-            'name': 'health',
-            'description': 'Health Monitoring',
+    }, {'name': 'health', 'description': 'Health Monitoring',
 
-        },
-        {
-            'name': 'production',
-            'description': 'Production Ready'
-        },
-        {
-            'name': 'admin',
-            'description': 'Admin Only '
-        }
-    ]
-)
+    }, {'name': 'production', 'description': 'Production Ready'}, {'name': 'admin', 'description': 'Admin Only '}])
 
 
+############ MISC METHODS############
+# https://fastapi.tiangolo.com/advanced/security/http-basic-auth/#check-the-username
+def authenticate(credentials: Annotated[HTTPBasicCredentials, Depends(security)]):
+    current_username_bytes = credentials.username.encode("utf8")
+    current_password_bytes = credentials.password.encode("utf8")
+
+    admin_username_bytes = ADMIN_USERNAME.encode("utf8")
+    if secrets.compare_digest(current_username_bytes, admin_username_bytes):
+        admin_password_bytes = ADMIN_PASSWORD.encode("utf8")
+        if secrets.compare_digest(current_password_bytes, admin_password_bytes):
+            return credentials.username
+
+    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect Username or Password")
 
 
-@app.get("/",tags=['production'])
+@app.get("/", tags=['production'])
 async def read_main():
-    msg = {"msg": "Welcome to the French Road Accident Project","opinion":"DataScientest Bootcamp is a scam"}
+    msg = {"msg": "Welcome to the French Road Accident Project", "opinion": "DataScientest Bootcamp is a scam"}
     logger.info(f"Saying Hello via msg {msg} ")
     return msg
 
 
-@app.get("/test/classifier_query",tags=['test'])
+@app.get("/test/classifier_query", tags=['test'])
 async def get_query(filter_query: Annotated[ClassifierRequest, Query()]):
     return filter_query
 
-@app.get("/test/forecaster_query",tags=['test'])
+
+@app.get("/test/forecaster_query", tags=['test'])
 async def get_query(filter_query: Annotated[ForecastRequest, Query()]):
     return filter_query
+
 
 ######REGION################
 
 ##Forecasting Endpoint
-@app.post("/predict/forecast",tags=['production'])
-async def forecast_accidents(request:Annotated[ForecastRequest, Query()]):
+@app.post("/predict/forecast", tags=['production'])
+async def forecast_accidents(request: Annotated[ForecastRequest, Query()]):
     try:
         arima_model = model_dic['arima_model']
         forecast = arima_model.predict(n_periods=request.periods)
@@ -182,16 +173,15 @@ async def forecast_accidents(request:Annotated[ForecastRequest, Query()]):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-@app.post("/predict/severity",tags=['production'])
-async def predict_severity(request:Annotated[ClassifierRequest, Query()]):
+
+@app.post("/predict/severity", tags=['production'])
+async def predict_severity(request: Annotated[ClassifierRequest, Query()]):
     try:
         hex_3 = h3.latlng_to_cell(request.latitude, request.longitude, H3_RESOLUTION)
         print(int(request.vehicle_category))
-        features = [[
-            request.vehicle_category, request.obstacle_mobile, request.impact_point,
-            request.action, request.safety_equipment, request.road_surface,
-            request.lum, request.weather, request.collision_type, request.speed_limit,request.accident_hex_count
-        ]]
+        features = [[request.vehicle_category, request.obstacle_mobile, request.impact_point, request.action,
+            request.safety_equipment, request.road_surface, request.lum, request.weather, request.collision_type,
+            request.speed_limit, request.accident_hex_count]]
         gbc_model = model_dic['gbc_model']
         prediction = gbc_model.predict(features)
         probability = gbc_model.predict_proba(features)[:, 1]
@@ -199,70 +189,70 @@ async def predict_severity(request:Annotated[ClassifierRequest, Query()]):
         return {"prediction": int(prediction[0]), "probability": float(probability[0])}
 
     except Exception as ex:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                            detail=str(ex))
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(ex))
 
 
 #############Admin#############
 @app.post("/admin/retrain", tags=['admin'])
-async def retrain_model(request:Annotated[AdminRequest, Query()]):
-    message = {
-        'message': f"Model Retraining Trigger is {request.retrain}"
-    }
+async def retrain_model(username: Annotated[str, Depends(authenticate)], request: Annotated[AdminRequest, Query()]):
+    if username != 'admin':
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Only Admin can post, Naughty!!!")
+    message = {'message': f"Model Retraining Trigger is {request.retrain}"}
     if bool(request.retrain):
         print("Retraining The Model")
-        #await load_file_as_module(name='module.name',location=ZENML_FILE_PATH)
-        asyncio.create_task(load_file_as_module('module.name', ZENML_FILE_PATH)) ## although blocking is a bit better to avoid overload
+        # await load_file_as_module(name='module.name',location=ZENML_FILE_PATH)
+        asyncio.create_task(
+            load_file_as_module('module.name', ZENML_FILE_PATH))  ## although blocking is a bit better to avoid overload
     return message
 
+
 ##############Health#########################
-@app.get("/health/status",tags=['health'],name="Status Check")
+@app.get("/health/status", tags=['health'], name="Status Check")
 async def health_check():
     _status = {"status": "API is running"}
     logger.info(f" Health Status {_status}")
     return _status
 
- ## This is stupid but the sham of a course from Datascientest asks for it ,If they only put the same motivation in designing their course it wont be a scam
-@app.get("/health/severity",tags=['health'],name="Severity Classifier Model Check")
-async def health_check_severity():
 
-    gbc_model  = model_dic['gbc_model']
+## This is stupid but the sham of a course from Datascientest asks for it ,If they only put the same motivation in designing their course it wont be a scam
+@app.get("/health/severity", tags=['health'], name="Severity Classifier Model Check")
+async def health_check_severity():
+    gbc_model = model_dic['gbc_model']
     if gbc_model is None:
-        raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE,detail="Severity Classifier not loaded")
+        raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail="Severity Classifier not loaded")
     try:
         request = generate_random_classifier_request()
-        features = [[
-            request.vehicle_category, request.obstacle_mobile, request.impact_point,
-            request.action, request.safety_equipment, request.road_surface,
-            request.lum, request.weather, request.collision_type, request.speed_limit,request.accident_hex_count
-        ]]
+        features = [[request.vehicle_category, request.obstacle_mobile, request.impact_point, request.action,
+            request.safety_equipment, request.road_surface, request.lum, request.weather, request.collision_type,
+            request.speed_limit, request.accident_hex_count]]
         gbc_model = model_dic['gbc_model']
         prediction = gbc_model.predict(features)
         probability = gbc_model.predict_proba(features)[:, 1]
-        _message = {"prediction": int(prediction[0]), "probability": float(probability[0]), "health":"Model is Healthy" if float(probability[0]) >0.25 else "Model Unhealthy"}
+        _message = {"prediction": int(prediction[0]), "probability": float(probability[0]),
+                    "health": "Model is Healthy" if float(probability[0]) > 0.25 else "Model Unhealthy"}
         return _message
     except Exception as ex:
-        raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
-                            detail=str(ex))
+        raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail=str(ex))
 
 
-@app.get("/health/forecast",tags=['health'],name="Forecast Model Check")
+@app.get("/health/forecast", tags=['health'], name="Forecast Model Check")
 async def health_check_forecast():
     arima_model = model_dic['arima_model']
     if arima_model is None:
         raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail="Forecast Model not loaded")
     try:
-        forecast = arima_model.predict(n_periods=69) ##Some random value
-        return {"forecast": forecast.tolist(), 'health':'Model is healthy' if len(forecast.tolist())==69 else "Model Unhealthy"}
+        forecast = arima_model.predict(n_periods=69)  ##Some random value
+        return {"forecast": forecast.tolist(),
+                'health': 'Model is healthy' if len(forecast.tolist()) == 69 else "Model Unhealthy"}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
 
 ##############RANDOM METHODS###############
 ##https://stackoverflow.com/questions/3781851/run-a-python-script-from-another-python-script-passing-in-arguments
-#https://stackoverflow.com/questions/436198/what-alternative-is-there-to-execfile-in-python-3-how-to-include-a-python-fil/16577427#16577427
-#https://stackoverflow.com/questions/67631/how-can-i-import-a-module-dynamically-given-the-full-path
-async def load_file_as_module(name='module.name',location=ZENML_FILE_PATH):
+# https://stackoverflow.com/questions/436198/what-alternative-is-there-to-execfile-in-python-3-how-to-include-a-python-fil/16577427#16577427
+# https://stackoverflow.com/questions/67631/how-can-i-import-a-module-dynamically-given-the-full-path
+async def load_file_as_module(name='module.name', location=ZENML_FILE_PATH):
     spec = importlib.util.spec_from_file_location(name, location)
     foo = importlib.util.module_from_spec(spec)
     sys.modules[name] = foo
@@ -274,6 +264,7 @@ async def load_file_as_module(name='module.name',location=ZENML_FILE_PATH):
 ##Random stuff
 def do_stuff():
     print("Hii")
+
 
 ##Startup
 # @app.on_event("startup")
@@ -297,7 +288,6 @@ def do_stuff():
 #         print(f"Error loading models: {e}")
 
 
-#TODO Remove this region
+# TODO Remove this region
 if __name__ == "__main__":
     do_stuff()
-
