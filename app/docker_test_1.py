29115b28857c41dbcfd38eb8d855932662cd6f42
fdevi3-mle <fdevi3@proton.me>
2025-03-12 13:23:07 +0100

    Reformated teh code

--- app/docker_test_1.py
+++ app/docker_test_1.py
@@ -7,7 +7,7 @@ import requests
 
 ###URLS
 ##TODO CHANGE TO base_url="http://fast_api:8000" for production
-base_url="http://127.0.0.1:8000"
+base_url = "http://127.0.0.1:8000"
 hello_url = base_url + "/"
 health_url = base_url + "/health/status"
 query_forecast_url = base_url + "/test/forecaster_query"
@@ -21,6 +21,7 @@ LOG_PATH = os.path.join(CURRENT_PATH, 'logs')
 
 logger = logging.getLogger(__name__)
 
+
 ### SIMULATES A NORMAL USER
 
 ##MISC Methods
@@ -32,7 +33,7 @@ def basic_auth(username, password):
 
 def generate_logger(_logpath=LOG_PATH, filename="default"):
     os.makedirs(LOG_PATH, exist_ok=True)
-    filename= f'{filename}.log'
+    filename = f'{filename}.log'
     filepath = os.path.join(_logpath, filename)
     logging.basicConfig(filename=filepath, encoding='utf-8', level=logging.DEBUG)
     logger.info(f"Hi, Logger Fired Up  for {filename}!!")
@@ -59,34 +60,33 @@ def read_health_status():
 
 
 def read_forecast_query():
-    params = {'periods':13}
-    response = requests.get(query_forecast_url,params=params)
-    logger.info(f"Docker 1 pokes @ {query_forecast_url} and receives {response.json()} with status code {response.status_code}")
-    print(f"Docker 1 pokes @ {query_forecast_url} and receives {response.json()} with status code {response.status_code}")
+    params = {'periods': 13}
+    response = requests.get(query_forecast_url, params=params)
+    logger.info(
+        f"Docker 1 pokes @ {query_forecast_url} and receives {response.json()} with status code {response.status_code}")
+    print(
+        f"Docker 1 pokes @ {query_forecast_url} and receives {response.json()} with status code {response.status_code}")
+
 
 def read_forecast_health():
-    response= requests.get(health_forecast_url)
-    logger.info(
-        f"Docker 1 pokes @ {health_forecast_url}  with status code {response.status_code}")
-    print(
-        f"Docker 1 pokes @ {query_forecast_url} with status code {response.status_code}")
+    response = requests.get(health_forecast_url)
+    logger.info(f"Docker 1 pokes @ {health_forecast_url}  with status code {response.status_code}")
+    print(f"Docker 1 pokes @ {query_forecast_url} with status code {response.status_code}")
     print(response.json())
+
 
 def read_severity_health():
-    response= requests.get(health_severity_url)
-    logger.info(
-        f"Docker 1 pokes @ {health_severity_url}  with status code {response.status_code}")
-    print(
-        f"Docker 1 pokes @ {health_severity_url} with status code {response.status_code}")
+    response = requests.get(health_severity_url)
+    logger.info(f"Docker 1 pokes @ {health_severity_url}  with status code {response.status_code}")
+    print(f"Docker 1 pokes @ {health_severity_url} with status code {response.status_code}")
     print(response.json())
-
 
 
 ### MAIN
 
-if __name__=="__main__":
+if __name__ == "__main__":
     generate_logger(filename='docker_1')
-    time.sleep(10) ## sleep till it all starts up
+    time.sleep(10)  ## sleep till it all starts up
     for i in range(5):
         read_main()
         time.sleep(1)
@@ -96,8 +96,6 @@ if __name__=="__main__":
         time.sleep(1)
         read_severity_health()
         time.sleep(1)
-
-
 
 #
 # ###OLD CODE
--- app/docker_test_2.py
+++ app/docker_test_2.py
@@ -7,7 +7,7 @@ import requests
 
 ###URLS
 ##TODO CHANGE TO base_url="http://fast_api:8000" for production
-base_url="http://127.0.0.1:8000"
+base_url = "http://127.0.0.1:8000"
 hello_url = base_url + "/"
 health_url = base_url + "/health/status"
 query_forecast_url = base_url + "/test/forecaster_query"
@@ -22,6 +22,7 @@ LOG_PATH = os.path.join(CURRENT_PATH, 'logs')
 
 logger = logging.getLogger(__name__)
 
+
 ### SIMULATES A NORMAL USER
 
 ##MISC Methods
@@ -33,7 +34,7 @@ def basic_auth(username, password):
 
 def generate_logger(_logpath=LOG_PATH, filename="default"):
     os.makedirs(LOG_PATH, exist_ok=True)
-    filename= f'{filename}.log'
+    filename = f'{filename}.log'
     filepath = os.path.join(_logpath, filename)
     logging.basicConfig(filename=filepath, encoding='utf-8', level=logging.DEBUG)
     logger.info(f"Hi, Logger Fired Up  for {filename}!!")
@@ -43,34 +44,38 @@ def generate_logger(_logpath=LOG_PATH, filename="default"):
 
 def read_main():
     response = requests.get(hello_url)
-    logger.info(f" Docker 1 says : {response.json()} ")
+    logger.info(f" Docker 2 says : {response.json()} ")
 
 
 def read_health_status():
     response = requests.get(health_url)
-    logger.info(f" Docker 1 pokes health @ {health_url} and receives {response.json()}")
-    print(f" Docker 1 pokes health @ {health_url} and receives {response.json()}")
+    logger.info(f" Docker 2 pokes health @ {health_url} and receives {response.json()}")
+    print(f" Docker 2 pokes health @ {health_url} and receives {response.json()}")
 
 
 def read_permission():
-    auth = basic_auth('admin','admin')
-    response = requests.get(permission_url,headers={"Authorization": auth})
-    logger.info(f"Docker 1 pokes @ {permission_url} and receives {response.json()} with status code {response.status_code}")
-    print(f"Docker 1 pokes @ {permission_url} and receives {response.json()} with status code {response.status_code}")
+    auth = basic_auth('admin', 'admin')
+    response = requests.get(permission_url, headers={"Authorization": auth})
+    logger.info(
+        f"Docker 2 pokes @ {permission_url} and receives {response.json()} with status code {response.status_code}")
+    print(f"Docker 2 pokes @ {permission_url} and receives {response.json()} with status code {response.status_code}")
+
 
 def do_retraining(retrain=False):
     auth = basic_auth('admin', 'admin')
-    params = {"retrain":retrain}
-    response = requests.post(admin_retrain_url,params=params,headers={"Authorization": auth})
-    logger.info(f"Docker 1 pokes @ {admin_retrain_url} and receives {response.json()} with status code {response.status_code}")
-    print(f"Docker 1 pokes @ {admin_retrain_url} and receives {response.json()} with status code {response.status_code}")
+    params = {"retrain": retrain}
+    response = requests.post(admin_retrain_url, params=params, headers={"Authorization": auth})
+    logger.info(
+        f"Docker 2 pokes @ {admin_retrain_url} and receives {response.json()} with status code {response.status_code}")
+    print(
+        f"Docker 2 pokes @ {admin_retrain_url} and receives {response.json()} with status code {response.status_code}")
 
 
 ### MAIN
 
-if __name__=="__main__":
-    generate_logger(filename='docker_1')
-    time.sleep(30) ## sleep till it all starts up
+if __name__ == "__main__":
+    generate_logger(filename='docker_2')
+    time.sleep(30)  ## sleep till it all starts up
     for i in range(3):
         read_main()
         time.sleep(1)
@@ -78,9 +83,7 @@ if __name__=="__main__":
         time.sleep(1)
         read_permission()
         time.sleep(1)
-    do_retraining(True) ## Do iit once only
-
-
+    do_retraining(True)  ## Do iit once only
 
 #
 # ###OLD CODE
