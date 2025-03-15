602522dccf102678fb8126b8cfb5c679661d6361
fdevi3-mle <fdevi3@proton.me>
2025-03-15 20:23:11 +0100

    Update docker_1_test.py

--- app/docker_1_test.py
+++ app/docker_1_test.py
@@ -87,13 +87,13 @@ def read_severity_health():
 if __name__ == "__main__":
     generate_logger(filename='docker_1')
     time.sleep(60)  ## sleep till it all starts up
-    for i in range(100):
+    for i in range(50):
         read_main()
-        time.sleep(5)
+        time.sleep(2)
         read_health_status()
-        time.sleep(5)
+        time.sleep(2)
         read_forecast_query()
-        time.sleep(5)
+        time.sleep(2)
         read_severity_health()
         time.sleep(10)
 
