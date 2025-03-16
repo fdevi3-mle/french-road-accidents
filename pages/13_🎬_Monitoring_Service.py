309d22ccb8189d9cb03ee4dd24aa9e3a9034eba9
fdevi3-mle <fdevi3@proton.me>
2025-03-16 20:59:20 +0100

    All pages added

--- pages/13_🎬_Monitoring_Service.py
+++ pages/13_🎬_Monitoring_Service.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-from src.utils import FIGURE_8, FIGURE_11, FIGURE_30
+from src.utils import FIGURE_8, FIGURE_11, FIGURE_30, FIGURE_26
 
 st.set_page_config(
     page_title="Monitoring Service with Prometheus and Grafana",
@@ -9,23 +9,35 @@ st.set_page_config(
 )
 st.write("# Monitoring Service with Prometheus and Grafana 🎬")
 st.markdown("""
-Once the UD Model Training was done and the Models were uploaded to the _CometML_ model registry. We needed to create an Inference Service where the user can query for
-Severity Classifications & Forecast Predictions. To build such a system we employed a relatively cool architecture (see image below) where each component was containerized in a _Docker_ system.
+To keep track of the Model and monitor the Services of the Inference Endpoint , I created a _Grafana_ Dashboard which scrapes the `Fast_API` service via prometheus and then displays the necessary 
+metrics and requests.
+
++ Prometheus : Using a `prometheus_fastapi_instrumentator` I scrape the _Fast API_ endpoints and `metrics`
++ Grafana : The Prometheus scrapes are then logged into a Dashboard (See below)
 """)
 
-st.write("##")
-st.image(FIGURE_30, caption="Figure: Road Accident Service Architecture Diagram", width =1400)
+st.write("#####")
+st.subheader("Prometheus Fast API Instrumentation")
+l1_col,r1_col = st.columns(2, border=True)
+
+with l1_col:
+    code_snippet = '''
+    from prometheus_fastapi_instrumentator import Instrumentator
+    Instrumentator().instrument(app).expose(app)
+    '''
+    st.code(code_snippet, language="python")
+
+with r1_col:
+    st.subheader("Prometheus Yaml Config Snippet")
+    code_snippet = '''
+      - job_name: 'fast_api'
+    scrape_interval: 2s
+    metrics_path: /metrics
+    static_configs:
+      - targets: ['fast_api:8000']
+    '''
+    st.code(code_snippet, language="yaml")
 st.divider()
-st.markdown("""
-In the next few pages we will go over the main components of the __Road Accident Architecture Service__ Diagram as listed below
-1. REST API Endpoints
-    + Inference Endpoint:
-        - "/predict/forecast"
-        - "/predict/severity"
-    + Admin Endpoint: via security clearance only
-        - "/admin"
-    + Metrics Endpoint with "/metrics"
-2. Prometheus Service for "/metrics" scrapping
-3. Grafana Service with Dashboard for Monitoring
-4. Test Clients (both User and Admin) just for testing
-""")
+st.write("##")
+st.image(FIGURE_26, caption="Figure: Grafana Dashboard", width =1400)
+
